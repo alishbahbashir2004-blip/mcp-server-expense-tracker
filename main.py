a6f2d4ff62e131c3d91e182c1e3f1cc866765718
alishbahbashir2004-blip <alishbahbashir2004@gmail.com>
2026-04-11 13:50:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import aiosqlite 
 import tempfile
 TEMP_DIR = tempfile.gettempdir()
-DB_PATH = os.path.join(os.path.dirname(__file__), "expenses.db")
+DB_PATH = os.path.join(TEMP_DIR, "expenses.db")
 CATEGORIES_PATH = os.path.join(os.path.dirname(__file__), "categories.json")
 
 print(f"Database path: {DB_PATH}")
